d92d8af4653792cb64a9734849ef9a4cf7b48828
Asmaagaber89782 <asmaagaber20192019gmail.com>
2023-02-23 18:47:40 -0800

    1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
-	#include "main.h"
-	/**
-	* main - Test function for positive or negative
-	* Return: 0
-	*/
-	int main(void)
-	{
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
-	return (0);
-	}
 
+	return (0);
+}
